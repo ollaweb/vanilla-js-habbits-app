a93b8378cd188fb9a55a2a0c62b96d536b281908
Olga L <huzijm@yandex.ru>
2023-05-15 17:33:10 +0300

    feature: render habbit's comments

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,10 @@ const page = {
         h1: document.querySelector('.header h1'),
         progressPercent: document.querySelector('.progress__percent'),
         progressCoverBar: document.querySelector('.progress__cover-bar')
+    },
+    content: {
+        daysContainer: document.querySelector('.habbits'),
+        nextDay: document.querySelector('main .habbit .habbit__day')
     }
 }
 
@@ -65,6 +69,37 @@ function rerenderHeader(activeHabbit) {
     page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
 }
 
+function rerenderContent(activeHabbit) {
+    page.content.daysContainer.innerHTML = '';
+    for (const index in activeHabbit.days) {
+        const element = document.createElement('li');
+        element.classList.add('habbit');
+        element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
+        <div class="habbit__body">
+        <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+      <button class="habbit__delete">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18 6V18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6M4 6H20M10 10V16M14 10V16"
+            stroke="#94A3BD"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+        </div>`;
+        page.content.daysContainer.appendChild(element);
+        page.content.nextDay.innerText = `День ${activeHabbit.days.length + 1}`;
+    }
+}
+
 function rerender(activeHabbitId) {
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
     if (!activeHabbit) {
@@ -72,6 +107,7 @@ function rerender(activeHabbitId) {
     }
     rerenderMenu(activeHabbit);
     rerenderHeader(activeHabbit);
+    rerenderContent(activeHabbit);
 }
 
 //Immediately Invoked Function Expression(IIFE) for init
