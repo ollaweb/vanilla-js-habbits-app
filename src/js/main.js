22a274cdf4824c4219c7b6b62ff64754be06dc58
Olga L <huzijm@yandex.ru>
2023-05-15 16:19:39 +0300

    feature: getting data from local storage when init app

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
 import '@/scss/main.scss'
 
+'use strict'
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+//utils
+function loadData() {
+    const habbitString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+//Immediately Invoked Function Expression(IIFE) for init
+(() => {
+    loadData();
+})();
